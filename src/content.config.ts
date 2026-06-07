4b87743310791ff426cc4f90e0453f9ca2ec625e
Bernardo Nogueira <samuraiflamesf@gmail.com>
2026-06-07 12:58:25 -0300

    Fix(update): Ao atualizar o astro tive problemas

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,16 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders'; // 1. Importe o loader
 
 const projectsCollection = defineCollection({
-  type: 'content',
+  // 2. Substitua o "type" pelo "loader", indicando o caminho da sua pasta de projetos
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
+  
   schema: z.object({
     title: z.string(),
+    description: z.string(),
     image: z.string(),
     objectives: z.string(),
     link: z.string(),
-    description: z.string(), 
   }),
 });
 
